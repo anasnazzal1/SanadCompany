d2ea56b31b01668419ae1be2a48444ecad6b75d9
Anas nazzal <ansnzal908@gmail.com>
2025-05-30 19:36:13 +0300

    add language to small screen

--- assets/js/lang-toggle.js
+++ assets/js/lang-toggle.js
@@ -16,3 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = newPath;
   });
 });
+
+// assets/lang-toggle.js to small screen
+document.addEventListener("DOMContentLoaded", function () {
+  const langToggle = document.getElementById("langToggle1");
+  const path = window.location.pathname;
+
+  // تحديد اللغة الحالية من الرابط
+  const currentLang = path.includes("/en/") ? "en" : "ar";
+  const newLang = currentLang === "en" ? "ar" : "en";
+
+  // تحديث نص الكبسة
+  langToggle.textContent = currentLang === "en" ? "العربية" : "English";
+
+  langToggle.addEventListener("click", () => {
+    // تحويل المسار للغة الجديدة
+    const newPath = path.replace(`/${currentLang}/`, `/${newLang}/`);
+    window.location.href = newPath;
+  });
+});
